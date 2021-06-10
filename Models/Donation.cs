12436271766af7d816796a48ee4f2845bf055345
Tuan Do <tuan.do@godtc.com>
2021-06-10 10:41:32 -0700

    Add project files.

---
+++ Models/Donation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ld_sample_app.Models
+{
+    public class Donation
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Server;
+using LaunchDarkly.Sdk.Server.Interfaces;
+using ld_sample_app.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ld_sample_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILdClient _ldClient;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILdClient ldClient, ILogger<IndexModel> logger)
+        {
+            _ldClient = ldClient;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public Donation Donation { get; set; }
+
+        [TempData]        
+        public bool FF_DONATE_BUTTON_ON_RIGHT { get; set; }
+
+        public IActionResult OnGet()
+        {
+            User user = LaunchDarkly.Sdk.User.Builder(Guid.NewGuid().ToString())
+                .Anonymous(true)
+                .Build();
+            FF_DONATE_BUTTON_ON_RIGHT = _ldClient.BoolVariation("donate-button-on-right", user, false);
+            //_logger.LogInformation("Feature flag is {flag-value}", FF_DONATE_BUTTON_ON_RIGHT);
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {            
+            // Submit donation and log experiment
+            _logger.LogInformation("Donated {donation-ammount} when feature flag is {flag-value}", Donation.Amount, FF_DONATE_BUTTON_ON_RIGHT);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
